705e116eea5e926ec09a698c41e363102c807617
justin <nninnnin7@gmail.com>
2023-11-28 00:41:18 +0900

    isStarted 추가

--- index.ts
+++ index.ts
@@ -1,73 +1,83 @@
-import { getTime } from "date-fns";
-import { useState, useRef } from "react";
+import { getTime } from 'date-fns'
+import { useState, useRef } from 'react'
 
 interface useTimerOutput {
-  timeLeft: number;
-  isOver: boolean;
-  reset: (stopConditionCallback: () => boolean) => void;
-  stop: () => void;
+  timeLeft: number
+  isStarted: boolean
+  isOver: boolean
+  reset: (stopConditionCallback: () => boolean) => void
+  stop: () => void
 }
 
 const useTimer = (timeLeftAsSecond: number): useTimerOutput => {
-  const endAtRef = useRef<number>(null);
-  const intervals = useRef<Array<NodeJS.Timeout>>([]);
+  const endAtRef = useRef<number>(null)
+  const intervals = useRef<Array<NodeJS.Timeout>>([])
 
-  const [timeLeft, setTimeLeft] = useState<number>(0);
-  const [isOver, setIsOver] = useState<boolean>(false);
+  const [timeLeft, setTimeLeft] = useState<number>(0)
+  const [isStarted, setIsStarted] = useState<boolean>(false)
+  const [isOver, setIsOver] = useState<boolean>(false)
 
   function processTime(callback: () => boolean) {
     const timeout = setTimeout(() => {
-      setTimeLeft((prev) => prev - 1000);
+      setTimeLeft((prev) => prev - 1000)
 
-      const currentTime = getTime(new Date());
-      const isOver = callback() || endAtRef.current <= currentTime;
+      const currentTime = getTime(new Date())
+      const isOver = callback() || endAtRef.current <= currentTime
 
       if (isOver) {
-        setIsOver(true);
+        setIsOver(true)
+        setIsStarted(false)
 
-        return;
+        return
       }
 
-      processTime(callback);
-    }, 1000);
+      processTime(callback)
+    }, 1000)
 
-    intervals.current.push(timeout);
+    intervals.current.push(timeout)
   }
 
   // 처음에 설정되었던 시간으로 되돌려 카운트다운을 재시작한다
   const reset = (stopConditionCallback: () => boolean) => {
     if (intervals.current.length) {
-      intervals.current.forEach((timeoutFunc) => clearTimeout(timeoutFunc));
+      intervals.current.forEach((timeoutFunc) =>
+        clearTimeout(timeoutFunc)
+      )
     }
 
-    setIsOver(false);
+    setIsStarted(true)
+    setIsOver(false)
 
-    const currentTime = getTime(new Date());
-    const timeLeft = timeLeftAsSecond * 1000;
+    const currentTime = getTime(new Date())
+    const timeLeft = timeLeftAsSecond * 1000
 
-    endAtRef.current = currentTime + timeLeft;
-    setTimeLeft(timeLeft);
+    endAtRef.current = currentTime + timeLeft
+    setTimeLeft(timeLeft)
 
-    processTime(stopConditionCallback);
-  };
+    processTime(stopConditionCallback)
+  }
 
   // 남은 시간은 그대로 두고 시계를 멈춘다
   const stop = () => {
     if (intervals.current.length) {
-      intervals.current.forEach((timeoutFunc) => clearTimeout(timeoutFunc));
+      intervals.current.forEach((timeoutFunc) =>
+        clearTimeout(timeoutFunc)
+      )
     }
 
-    setIsOver(true);
-  };
+    setIsStarted(false)
+    setIsOver(true)
+  }
 
   return {
     timeLeft,
+    isStarted,
     isOver,
     reset,
     stop,
-  };
-};
+  }
+}
 
 module.exports = {
   useTimer,
-};
+}
